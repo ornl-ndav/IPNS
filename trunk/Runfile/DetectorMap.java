326b473b5ae48d60bd740622caab401c31d09f8e
hammonds <hammonds@72994b36-f711-0410-b3c6-d20b2a964469>
2000-02-16 01:15:19 +0000

    Added code to handle glad LPSDs.  This will replace the LpsdMap class which was used to model LPSD functionality.

--- trunk/Runfile/DetectorMap.java
+++ trunk/Runfile/DetectorMap.java
@@ -11,45 +11,47 @@ Access to members is limited to members of the package.
 
 class DetectorMap{
 
-int address;
-int tfType;
-int moreHistBit;
+    int address;
+    int tfType;
+    int moreHistBit;
 
+    String iName;
+    /**
+       This function provides a test method for this class' functionality.  It
+       will provide a sampling of the information that is retrieved as a new 
+       TimeField Object is created.  It accepts a filename as the first command
+       line argument.
 
-/**
-This function provides a test method for this class' functionality.  It will
-provide a sampling of the information that is retrieved as a new TimeField
-Object is created.  It accepts a filename as the first command line argument.
-
-@param args - The first command line parameter is the runfile name.  This
-              parameter should contain the file path unless the file is in
+       @param args - The first command line parameter is the runfile name.  
+              This parameter should contain the file path unless the file is in
               the current directory.
 
-*/
-public static void main(String[] args) throws IOException {
-  int i;
-  int numEntries;
+    */
+    public static void main(String[] args) throws IOException {
+	int i;
+	int numEntries;
 
         RandomAccessFile runfile = new RandomAccessFile(
-                args[0], "r");
+							args[0], "r");
  	int slashIndex = args[0]
 	    .lastIndexOf( System.getProperty( "file.separator"));
 	String iName = args[0].substring( slashIndex+1, slashIndex + 5 );
-       Header header = new Header(runfile, iName );
+	Header header = new Header(runfile, iName );
         numEntries = header.detectorMapTable.size / 4;
         System.out.println("Number of Time Field entries in the table: " +
-                        numEntries);
+			   numEntries);
         DetectorMap[] detectorMap = new DetectorMap[numEntries+1];
         for (i=1; i <= numEntries; i++) {
-          detectorMap[i]  = new DetectorMap(runfile, i, header);
-          System.out.println( detectorMap[i].address + " " +
-		detectorMap[i].tfType + " " + detectorMap[i].moreHistBit);
-          }
+	    detectorMap[i]  = new DetectorMap(runfile, i, header);
+	    System.out.println( detectorMap[i].address + " " +
+				detectorMap[i].tfType + " " + 
+				detectorMap[i].moreHistBit);
+	}
         runfile.close();
-}
+    }
 
-protected  DetectorMap(RandomAccessFile runfile, int id, 
-				 Header header ) throws IOException{
+    protected  DetectorMap(RandomAccessFile runfile, int id, 
+			   Header header ) throws IOException{
 	long startingPosition;
 	int temp;
 
@@ -62,23 +64,43 @@ protected  DetectorMap(RandomAccessFile runfile, int id,
 	else {
 	    temp = runfile.readInt();
 	}
+	
+	iName = new String( header.iName );
 
-	address = temp & 0x7FFFFF;
-	tfType = (temp >> 24) & 0xFF;
-        moreHistBit = (temp >> 23) & 0x1;
-/*	System.out.println("DMap " + id + " " + address + " " + tfType + " " 
-				+ moreHistBit);
-*/
-	runfile.seek(startingPosition);
+	if ( !iName.equalsIgnoreCase("glad") &&
+	     !iName.equalsIgnoreCase("lpsd") ) {
+
+	    address = temp & 0x7FFFFF;
+	    tfType = (temp >> 24) & 0xFF;
+	    moreHistBit = (temp >> 23) & 0x1;
 	}
+	else {
+
+	    address = temp & 0xFFFF;
+	    address = address << 8;
+	    tfType = (temp >> 24) & 0xFF;
+	    moreHistBit = (temp >> 23) & 0x1;
+	}	    
+	    
+	runfile.seek(startingPosition);
+    }
 
     protected DetectorMap() {
     }
 
     protected void Write ( RandomAccessFile runfile ) throws IOException {
-	int tfEntry = ( address & 0x7FFFFF ) | ( (tfType << 24) & 0xFF000000 )
-	    | ( (moreHistBit << 23)  &  0x800000 );
-	runfile.writeInt( tfEntry );
+	int tfEntry;
+	if ( !iName.equalsIgnoreCase("glad") &&
+	     !iName.equalsIgnoreCase("lpsd") ) {
+
+	    tfEntry = ( address & 0x7FFFFF ) | ( (tfType << 24) & 0xFF000000 )
+		| ( (moreHistBit << 23)  &  0x800000 );
+	}
+	else {
+	    tfEntry = ( address & 0x7FFFFF ) | ( (tfType << 24) & 0xFF000000 )
+		| ( (moreHistBit << 23)  &  0x800000 );
+	}
+	    runfile.writeInt( tfEntry );
 	
     }
 
