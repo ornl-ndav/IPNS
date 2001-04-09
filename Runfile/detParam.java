65e356cbcdb2e6d15d1c0d452d540918ef2c56fa
John Hammonds <JPHammonds@users.noreply.github.com>
2001-04-09 18:36:16 +0000

    Added functions for dataSource and minID.

--- Runfile/detParam.java
+++ Runfile/detParam.java
@@ -34,8 +34,10 @@ information.
 */
 
 public detParam(Runfile runFile){
-   Object[][] detParamList = new Object[runFile.NumDet()][9];
+    Object[][] detParamList = new Object[runFile.NumDet()][11];
+   System.out.println(runFile.NumDet() );
    for (int i = 0; i < runFile.NumDet(); i++) {
+       System.out.println( "ID: " + i );
      detParamList[i][0] = new Integer(i +1);
      detParamList[i][1] = new Float((float)runFile.RawFlightPath( i + 1 ));
      detParamList[i][2] = new Float((float)runFile.FlightPath( i + 1, (int)1 ));
@@ -45,14 +47,27 @@ public detParam(Runfile runFile){
      detParamList[i][6] = new Integer((short)runFile.DetectorType( i + 1 ));
      detParamList[i][7] = new Integer((int)runFile.LowerLevelDisc(i + 1));
      detParamList[i][8] = new Integer((int)runFile.UpperLevelDisc(i + 1));
+     detParamList[i][9] = new Integer((int)runFile.DataSource(i + 1));
+     detParamList[i][10] = new Integer((int)runFile.MinID(i + 1));
+     
      }
 
    String[] columnHeading = {"ID", "Raw Length", "Length", "Raw Angle",
 				"Angle", "Height", "Type", "Lower Disc",
-				"Upper Disc"};
+				"Upper Disc","Data Source", "Min ID"};
 
    detParamJTable = new JTable(detParamList, columnHeading);
    setViewportView(detParamJTable);
 	  
    }
+   
+    public static void main(String[] args ) throws IOException {
+	Runfile runFile = new Runfile(args[0]);
+
+	JFrame mainFrame = new JFrame();
+	detParam dParam = new detParam( runFile );
+	mainFrame.getContentPane().add(dParam);
+	mainFrame.pack();
+	mainFrame.show();
+    }
 }
