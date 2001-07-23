8b542cb232c19d03778412bc0987c76b3864fae5
John Hammonds <JPHammonds@users.noreply.github.com>
2001-07-23 21:19:35 +0000

    Added to support newrun type scripts without iCame

--- Runfile/PhysicalConstants.java
+++ Runfile/PhysicalConstants.java
@@ -8,46 +8,6 @@ These Constants were taken from:
 "The NIST Reference on Cunstants, Units and Uncertainty
 http://physics.nist.gov/cuu/Constants/index.html?/codata86.html
 
-    neutron mass 
-         Value                                 1.674 927 16 x 10-27 kg 
-         Standard uncertainty                  0.000 000 13 x 10-27 kg 
-         Relative standard uncertainty         7.9 x 10-8
-         Concise form                          1.674 927 16(13) x 10-27 kg 
-   joule-electron volt relationship 
-         Value                                 6.241 509 74 x 10+18 eV 
-         Standard uncertainty                  0.000 000 24 x 1018 eV 
-         Relative standard uncertainty         3.9 x 10-8
-         Concise form                          6.241 509 74(24) x 1018 eV
-   electron volt-joule relationship 
-         Value                                 1.602 176 462 x 10-19 J 
-         Standard uncertainty                  0.000 000 063 x 10-19 J 
-         Relative standard uncertainty         3.9 x 10-8
-         Concise form                          1.602 176 462(63) x 10-19 J
-   Avogadro constant 
-         Value                                 6.022 141 99 x 1023 mol-1 
-         Standard uncertainty                  0.000 000 47 x 1023 mol-1 
-         Relative standard uncertainty         7.9 x 10-8
-         Concise form                          6.022 141 99(47) x 1023 mol-1
-   Bohr magneton in eV/T 
-         Value                                 5.788 381 749 x 10-5 eV T-1 
-         Standard uncertainty                  0.000 000 043 x 10-5 eV T-1 
-         Relative standard uncertainty         7.3 x 10-9
-         Concise form                          5.788 381 749(43) x 10-5 eV T-1
-   nuclear magneton in eV/T 
-         Value                                 3.152 451 238 x 10-8 eV T-1 
-         Standard uncertainty                  0.000 000 024 x 10-8 eV T-1 
-         Relative standard uncertainty         7.6 x 10-9
-         Concise form                          3.152 451 238(24) x 10-8 eV T-1
-   neutron magnetic moment to nuclear magneton ratio 
-         Value                                -1.913 042 72 
-         Standard uncertainty                  0.000 000 45 
-         Relative standard uncertainty         2.4 x 10-7
-         Concise form                         -1.913 042 72(45)    
-   Planck constant 
-         Value                                 6.626 068 76 x 10-34 J s 
-         Standard uncertainty                  0.000 000 52 x 10-34 J s
-         Relative standard uncertainty         7.8 x 10-8
-         Concise form                          6.626 068 76(52) x 10-34 J s
 
 
 @author  John P Hammonds
@@ -56,34 +16,136 @@ http://physics.nist.gov/cuu/Constants/index.html?/codata86.html
 /* 
  *
  * $Log$
+ * Revision 5.1  2001/07/23 21:18:38  hammonds
+ * Added to support newrun type scripts without iCame
+ *
  * Revision 5.0  2000/03/10 04:13:40  hammonds
  * New Class to localize definitions of Physics constants.
+These Constants were taken from:
+"The NIST Reference on Cunstants, Units and Uncertainty
+http://physics.nist.gov/cuu/Constants/index.html?/codata86.html
+   Planck constant 
+         Value                                 6.626 068 76 x 10-34 J s 
+         Standard uncertainty                  0.000 000 52 x 10-34 J s
+         Relative standard uncertainty         7.8 x 10-8
+         Concise form                          6.626 068 76(52) x 10-34 J s
+
  *
  *
  */
 
 public class PhysicalConstants {
 
+    /**
+These Constants were taken from:
+"The NIST Reference on Cunstants, Units and Uncertainty
+http://physics.nist.gov/cuu/Constants/index.html?/codata86.html
+    neutron mass 
+         Value                                 1.674 927 16 x 10-27 kg 
+         Standard uncertainty                  0.000 000 13 x 10-27 kg 
+         Relative standard uncertainty         7.9 x 10-8
+         Concise form                          1.674 927 16(13) x 10-27 kg 
+
+     */
     public static final double 
 	NEUTRON_MASS = 1.67492716E-27;
+    /**
+These Constants were taken from:
+"The NIST Reference on Cunstants, Units and Uncertainty
+http://physics.nist.gov/cuu/Constants/index.html?/codata86.html
+   joule-electron volt relationship 
+         Value                                 6.241 509 74 x 10+18 eV 
+         Standard uncertainty                  0.000 000 24 x 1018 eV 
+         Relative standard uncertainty         3.9 x 10-8
+         Concise form                          6.241 509 74(24) x 1018 eV
+
+     */
     public static final double 
 	ELECTRON_VOLT_PER_JOULE = 6.24150974E18;
+    /**
+These Constants were taken from:
+"The NIST Reference on Cunstants, Units and Uncertainty
+http://physics.nist.gov/cuu/Constants/index.html?/codata86.html
+   electron volt-joule relationship 
+         Value                                 1.602 176 462 x 10-19 J 
+         Standard uncertainty                  0.000 000 063 x 10-19 J 
+         Relative standard uncertainty         3.9 x 10-8
+         Concise form                          1.602 176 462(63) x 10-19 J
+     */
     public static final double 
 	JOULE_PER_ELECTRON_VOLT = 1.602176462E-19;
+    /**
+These Constants were taken from:
+"The NIST Reference on Cunstants, Units and Uncertainty
+http://physics.nist.gov/cuu/Constants/index.html?/codata86.html
+   Avogadro constant 
+         Value                                 6.022 141 99 x 1023 mol-1 
+         Standard uncertainty                  0.000 000 47 x 1023 mol-1 
+         Relative standard uncertainty         7.9 x 10-8
+         Concise form                          6.022 141 99(47) x 1023 mol-1
+
+     */
     public static final double 
 	AVOGADRO = 6.02214199E23;
+    /**
+These Constants were taken from:
+"The NIST Reference on Cunstants, Units and Uncertainty
+http://physics.nist.gov/cuu/Constants/index.html?/codata86.html
+   Bohr magneton in eV/T 
+         Value                                 5.788 381 749 x 10-5 eV T-1 
+         Standard uncertainty                  0.000 000 043 x 10-5 eV T-1 
+         Relative standard uncertainty         7.3 x 10-9
+         Concise form                          5.788 381 749(43) x 10-5 eV T-1
+
+     */
     public static final double 
 	BOHR_MAGNETON = 5.788381749E-5;
+    /**
+These Constants were taken from:
+"The NIST Reference on Cunstants, Units and Uncertainty
+http://physics.nist.gov/cuu/Constants/index.html?/codata86.html
+   nuclear magneton in eV/T 
+         Value                                 3.152 451 238 x 10-8 eV T-1 
+         Standard uncertainty                  0.000 000 024 x 10-8 eV T-1 
+         Relative standard uncertainty         7.6 x 10-9
+         Concise form                          3.152 451 238(24) x 10-8 eV T-1
+
+     */
     public static final double 
 	NUCLEAR_MAGNETON = 3.152451238E-8;
+    /**
+These Constants were taken from:
+"The NIST Reference on Cunstants, Units and Uncertainty
+http://physics.nist.gov/cuu/Constants/index.html?/codata86.html
+   neutron magnetic moment to nuclear magneton ratio 
+         Value                                -1.913 042 72 
+         Standard uncertainty                  0.000 000 45 
+         Relative standard uncertainty         2.4 x 10-7
+         Concise form                         -1.913 042 72(45)    
+
+     */
     public static final double 
 	NEUTRON_MAG_MOMENT_BY_NUCLEAR_MAGNETON = -1.91304272;
+    /**
+These Constants were taken from:
+"The NIST Reference on Cunstants, Units and Uncertainty
+http://physics.nist.gov/cuu/Constants/index.html?/codata86.html
+     */
     public static final double  
 	NEUTRON_MAGNETIC_MOMENT = 
 	NUCLEAR_MAGNETON * NEUTRON_MAG_MOMENT_BY_NUCLEAR_MAGNETON;
+    /**
+
+     */
     public static final double
 	eV_FROM_m_PER_sec_CONST = 
 	NEUTRON_MASS / ( 2.0 * JOULE_PER_ELECTRON_VOLT );
+    /**
+These Constants were taken from:
+"The NIST Reference on Cunstants, Units and Uncertainty
+http://physics.nist.gov/cuu/Constants/index.html?/codata86.html
+
+     */
     public static final double
 	meV_FROM_m_PER_microsec_CONST = 
 	eV_FROM_m_PER_sec_CONST  / ((1.0E-6)*(1.0E-6)* 1E-3) ;
