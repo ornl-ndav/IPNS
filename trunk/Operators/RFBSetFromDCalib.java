fba8830e801b55dc77f4a0fa7c5e60aacf48d146
chatter <chatter@72994b36-f711-0410-b3c6-d20b2a964469>
2001-08-01 20:57:32 +0000

    Added GPL license statement

--- trunk/Operators/RFBSetFromDCalib.java
+++ trunk/Operators/RFBSetFromDCalib.java
@@ -1,3 +1,45 @@
+/*
+ * File: RFBSetFromDCalib.java 
+ *
+ * Copyright (C) 2001, Alok Chatterjee,
+ *                     Ruth Mikkelson, 
+ *                     John Hammonds
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Alok Chatterjee achatterjee@anl.gov>
+ *           Intense Pulsed Neutron Source Division
+ *           Argonne National Laboratory
+ *           9700 S. Cass Avenue, Bldg 360
+ *           Argonne, IL 60440
+ *           USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
+ *
+ *  $Log$
+ *  Revision 1.2  2001/08/01 20:57:32  chatter
+ *  Added GPL license statement
+ *
+ *
+ */
+
 package IPNS.Operators;
 
 import  java.io.*;
