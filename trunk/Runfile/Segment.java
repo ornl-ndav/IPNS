22f00dd137660dc63a3a85ff9ffe8c60e9d2034b
hammonds <hammonds@72994b36-f711-0410-b3c6-d20b2a964469>
2001-08-03 19:02:53 +0000

    Added a segment ID to uniquely identify each element.

--- trunk/Runfile/Segment.java
+++ trunk/Runfile/Segment.java
@@ -12,6 +12,9 @@ element tubes, LPSDs and Area detectors.
 /*
  *
  * $Log$
+ * Revision 1.2  2001/08/03 19:02:53  hammonds
+ * Added a segment ID to uniquely identify each element.
+ *
  * Revision 1.1  2001/06/27 22:19:32  hammonds
  * Added to help implement area & lpsd data.
  *
@@ -22,7 +25,7 @@ public class Segment {
     int detID;
     int row;
     int column;
-
+    int segID;
     float length;
     float width;
     float depth;
@@ -33,7 +36,7 @@ public class Segment {
     }
 
     public Segment( int detID, int row, int column, float length, float width,
-		   float depth, float Efficiency ) {
+		   float depth, float Efficiency, int segID ) {
 	this.detID = detID;
 	this.row = row;
 	this.column = column;
@@ -41,6 +44,7 @@ public class Segment {
 	this.width = width;
 	this.depth = depth;
 	this.efficiency = efficiency;
+	this.segID = segID;
     }
     /**
        This method returns the detector ID for this segment
@@ -101,6 +105,14 @@ public class Segment {
      */
     public float Efficiency() {
 	return efficiency;
+    }
+
+    /**
+       This method returns the segment ID of this segment of the detector
+       @return efficiency
+     */
+    public int SegID() {
+	return segID;
     }
 
     
