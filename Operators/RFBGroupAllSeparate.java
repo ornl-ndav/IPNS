72fd62ee67f2b1fed0ce8db9c5af7de920bedfd2
chatterjee <chatterjee@72994b36-f711-0410-b3c6-d20b2a964469>
2001-11-01 20:37:38 +0000

    Added new method to allow grouping detectors separately

---
+++ Operators/RFBGroupAllSeparate.java
@@ -0,0 +1,125 @@
+/*
+ * File: RFBGroupAllSeparate.java 
+ *
+ * Copyright (C) 2001, Alok Chatterjee,
+ *                     Ruth Mikkelson, 
+ *                     John Hammonds
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Alok Chatterjee achatterjee@anl.gov>
+ *           Intense Pulsed Neutron Source Division
+ *           Argonne National Laboratory
+ *           9700 S. Cass Avenue, Bldg 360
+ *           Argonne, IL 60440
+ *           USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
+ *
+ *  $Log$
+ *  Revision 1.1  2001/11/01 20:37:38  chatterjee
+ *  Added new method to allow grouping detectors separately
+ *
+ *  Revision 1.2  2001/08/01 20:57:32  chatter
+ *  Added GPL license statement
+ *
+ *
+ */
+
+package IPNS.Operators;
+
+import  java.io.*;
+import  java.util.*;
+import  DataSetTools.operator.*;
+import  IPNS.Runfile.*;
+
+/**
+ * This operator instantiates IPNS.RunfileBuilder.
+ * 
+ */
+
+public class RFBGroupAllSeparate extends  GenericBatch 
+                            implements Serializable
+{
+
+  /* ------------------------ FULL CONSTRUCTOR -------------------------- */
+  /**
+   * Construct a default RFBGroupAllSeparate operator to to allow scripts to call IPNS.RunfileBuilder.
+   */
+  public RFBGroupAllSeparate( RunfileBuilder rfb, Integer val1, Integer val2)
+  {
+	
+    super( "RFBGroupAllSeparate" );
+    System.out.println("Inside the constructor now");  
+    Parameter parameter= new Parameter("Runfilebuilder", rfb );
+    addParameter( parameter );
+    parameter= new Parameter("TF", null );
+    addParameter( parameter );
+    parameter= new Parameter("Hist", null );
+    addParameter( parameter );
+  }
+
+public RFBGroupAllSeparate()
+  { super( "RFBGroupAllSeparate" );
+	//System.out.println("def constructor");
+     setDefaultParameters();
+  }
+
+  /* ---------------------------- getCommand ------------------------------- */
+  /**
+   * @return	the command name to be used with script processor: in this case, Pause
+   */
+   public String getCommand()
+   {
+     return "RFBGroupAllSeparate";
+   }
+
+ /* -------------------------- setDefaultParmeters ------------------------- */
+ /**
+  *  Set the default parameters.
+  */
+  public void setDefaultParameters()
+  {
+     parameters = new Vector();  // must do this to create empty list of 
+                                 // parameters
+    Parameter parameter= new Parameter("New RunfileBuilder", new RunfileBuilder() );
+    addParameter( parameter );
+    parameter= new Parameter("TF", null );
+    addParameter( parameter );
+    parameter= new Parameter("Hist", null );
+    addParameter( parameter );
+
+  }
+
+
+  /* ---------------------------- getResult ------------------------------- */
+
+  public Object getResult()
+  {
+    int rval = 0;
+    RunfileBuilder rfb = (RunfileBuilder) (getParameter(0).getValue());
+    int val1   = ( (Integer)(getParameter(1).getValue())).intValue();
+    int val2   = ( (Integer)(getParameter(2).getValue())).intValue();
+    rval = rfb.groupAllSeparate(val1, val2);    
+    return new Integer(rval);
+         
+  }  
+
+}
