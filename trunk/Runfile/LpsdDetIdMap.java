7a445941a072bfce68a6be0276a059c1d69883c6
hammonds <hammonds@72994b36-f711-0410-b3c6-d20b2a964469>
2002-10-28 22:58:50 +0000

    Speed/memory improvements.  This is from change of code structure.

--- trunk/Runfile/LpsdDetIdMap.java
+++ trunk/Runfile/LpsdDetIdMap.java
@@ -21,8 +21,9 @@ MapElement[][] map = new MapElement[0][0];
 	String iName = args[0].substring( slashIndex+1, slashIndex + 5 );
 	Header header = new Header(runfile, iName );
 	LpsdDetIdMap idMap = new LpsdDetIdMap( runfile, header );
+	int[] dets = new int[0];
 	for ( int ii = 0; ii < idMap.NumOfBanks(); ii++ ) {
-	    int[] dets = idMap.DetsInBank(ii);
+	    dets = idMap.DetsInBank(ii);
 	    System.out.println( "Bank " + ii + " has " );
 	    for ( int jj = 0; jj < dets.length; jj++ ) {
 		int crate = idMap.CrateForDet( ii, dets[jj] );
@@ -44,20 +45,24 @@ MapElement[][] map = new MapElement[0][0];
 	long startingPosition = runfile.getFilePointer();
 	runfile.seek( header.PSD_IDMap.location );
 	int mapIndex = 0;
+	int temp;
+	int bankNo, numDets;
+	int ii;
+	MapElement[][] tempMap = new MapElement[0][0];
 	while ( mapIndex < header.PSD_IDMap.size/2 ) {
-	    int bankNo = header.readUnsignedInteger( runfile, 2 );
-	    int numDets = header.readUnsignedInteger( runfile, 2 );
-	    for (int ii = 0; ii < 5; ii++ ) {
-		int temp = header.readUnsignedInteger( runfile, 2);
+	    bankNo = header.readUnsignedInteger( runfile, 2 );
+	    numDets = header.readUnsignedInteger( runfile, 2 );
+	    for (ii = 0; ii < 5; ii++ ) {
+		temp = header.readUnsignedInteger( runfile, 2);
 	    }
 	    mapIndex += 7;
 	    if ( bankNo > map.length - 1 ) {
-		MapElement[][] tempMap = new MapElement[bankNo + 1][];
+		tempMap = new MapElement[bankNo + 1][];
 		System.arraycopy(map, 0, tempMap, 0, map.length);
 		map = tempMap;
 	    }
 	    map[bankNo] = new MapElement[numDets];
-	    for ( int ii = 0; ii < numDets; ii++ ) {
+	    for ( ii = 0; ii < numDets; ii++ ) {
 		map[bankNo][ii] = new MapElement();
 		map[bankNo][ii].detID = header.readUnsignedInteger( runfile,
 								    2 );
@@ -69,7 +74,7 @@ MapElement[][] map = new MapElement[0][0];
 								    2 );
 		map[bankNo][ii].minID = header.readUnsignedInteger( runfile,
 								    2 );
-		int temp = header.readUnsignedInteger( runfile, 2);
+		temp = header.readUnsignedInteger( runfile, 2);
 		temp = header.readUnsignedInteger( runfile, 2);
 		mapIndex += 7;
 	    }
