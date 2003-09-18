fe32ec502dc7f007f0791addded3933197312eec
hammonds <hammonds@72994b36-f711-0410-b3c6-d20b2a964469>
2003-09-18 20:42:28 +0000

    New to print out control parameters.

---
+++ trunk/Operators/Print/PrintControl.py
@@ -0,0 +1,87 @@
+from IPNS.Operators import *
+from IPNS.Runfile import Runfile
+from IPNS.Control import ParameterFile
+
+class PrintControl(GenericOperator):
+
+    def setDefaultParameters(self):
+        self.super__clearParametersVector()
+        defInst = System.getProperty("Default_Instrument")
+        instChoice = ChoiceListPG("Instrument Name",defInst)
+        instChoice.addItem("hrcs")
+        instChoice.addItem("qens")
+        instChoice.addItem("glad")
+        instChoice.addItem("scd0")
+        instChoice.addItem("sepd")
+        instChoice.addItem("lrcs")
+        instChoice.addItem("sand")
+        instChoice.addItem("posy")
+        instChoice.addItem("pne0")
+        instChoice.addItem("sad1")
+        instChoice.addItem("sasi")
+        instChoice.addItem("hipd")
+        instChoice.addItem("gppd")
+        instChoice.addItem("chex")
+        instChoice.addItem("quip")
+        self.addParameter(instChoice)
+        self.addParameter(IntegerPG("Enter Run Number: ", 0))
+
+    def getResult(self):
+        inst = self.getParameter(0).value
+        dd = Datadir(inst)
+
+        dataDir = dd.getResult()
+        runNum = self.getParameter(1).value
+        fName =""
+        sRun = ""
+        if runNum < 1000:
+            sRun = "%04d"%(runNum)
+        else:
+            sRun = "%d"%(runNum)
+        fName = "%s/%s%s.run"%(dataDir,inst,sRun)
+        runfile = Runfile(fName)
+        firstRun = runfile.FirstRun()
+        curRun = firstRun
+        if curRun < 1000:
+            sRun = "%04d"%(curRun)
+        else:
+            sRun = "%d"%(curRun)
+        fName = "%s/%s%s.run"%(dataDir,inst,sRun)
+        runfile = Runfile(fName)
+        done = 0
+        while done != 1:
+            userName = runfile.UserName()
+            numPulses = runfile.NumOfPulses()
+            runTitle = runfile.RunTitle()
+            params = runfile.getControlParams()
+            numControl = runfile.NumOfControl()
+            
+            print "********************************************"
+            print "Instrument: ", inst, "  Run #: ", curRun
+            print "User: ", userName
+            print "RunTitle:"
+            print runTitle
+            for x in range(numControl):
+                params[x].printDeviceBrief()
+                params[x].printUserParametersBrief()
+            nextRun = runfile.NextRun()
+            lastRun = runfile.LastRun()
+            print "********************************************"
+            if curRun == lastRun:
+                done = 1
+            else:
+                curRun = nextRun
+                if curRun < 1000:
+                    sRun = "%04d"%(curRun)
+                else:
+                    sRun = "%d"%(curRun)
+                    fName = "%s/%s%s.run"%(dataDir,inst,sRun)
+                    runfile = Runfile(fName)
+
+# Override Category to dictate where this appears in the Macro menu
+    def getCategoryList(self):
+        cat = ["Operator","DAS","Print"]
+        return cat
+
+    def __init__(self):
+        Operator.__init__(self,"Print Control")
