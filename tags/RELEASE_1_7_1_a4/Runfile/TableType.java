6d462772a766e6117fd90d5c46d515a785ae70f7
nobody <nobody@72994b36-f711-0410-b3c6-d20b2a964469>
2004-04-19 15:33:50 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_1_7_1_a4'.

---
+++ tags/RELEASE_1_7_1_a4/Runfile/TableType.java
@@ -0,0 +1,12 @@
+package IPNS.Runfile;
+
+/**
+This class is a utility class for the IPNS.Runfile package.  This class
+creates the equivalent to a C structure for holding the location and size
+of a table in an IPNS Runfile.  Access is limited to members of the package.
+
+*/
+class TableType{
+public int size;
+public int location;
+}
