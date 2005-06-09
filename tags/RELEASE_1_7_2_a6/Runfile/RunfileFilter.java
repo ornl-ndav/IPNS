b36b6f8bc2e48248c75776aeb7086e543667fdb9
nobody <nobody@72994b36-f711-0410-b3c6-d20b2a964469>
2005-06-09 16:07:15 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_1_7_2_a6'.

---
+++ tags/RELEASE_1_7_2_a6/Runfile/RunfileFilter.java
@@ -0,0 +1,31 @@
+package IPNS.Runfile;
+import java.io.File;
+import javax.swing.*;
+import javax.swing.filechooser.*;
+
+public class RunfileFilter extends FileFilter {
+   public boolean accept(File f){
+     if (f.isDirectory() ) {
+        return true;
+        }
+     String s = f.getName();
+     int index = s.lastIndexOf('.');
+     String extension = null;
+     if ( index > 0 && index < s.length() - 1 ) {
+        extension = s.substring(index +1).toLowerCase();
+        }
+     if ( extension != null ) {
+       if ( extension.equals("run") ){
+           return true;
+           }
+       else {
+           return false;
+           }
+       }
+     return false;
+     }
+
+public String getDescription() {
+     return "IPNS Runfile (*.run)";
+     }
+}
